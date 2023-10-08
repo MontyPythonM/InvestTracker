95561990ffa0d9b04a21c993fc848a2ddeb51c9c
Mateusz Szewczyk <mat.szewczyk96@gmail.com>
2023-10-08 14:15:36 +0200

    Implement stakeholder repository. Add UserRoleGranted and UserRoleRemoved event handlers in InvestmentStrategy module

---
+++ src/Modules/InvestmentStrategies/src/InvestTracker.InvestmentStrategies.Application/Stakeholders/Events/External/Handlers/UserRoleGrantedHandler.cs
@@ -0,0 +1,26 @@
+﻿using InvestTracker.InvestmentStrategies.Domain.Stakeholders.Repositories;
+using InvestTracker.Shared.Abstractions.IntegrationEvents;
+
+namespace InvestTracker.InvestmentStrategies.Application.Stakeholders.Events.External.Handlers;
+
+internal sealed class UserRoleGrantedHandler : IEventHandler<UserRoleGranted>
+{
+    private readonly IStakeholderRepository _stakeholderRepository;
+
+    public UserRoleGrantedHandler(IStakeholderRepository stakeholderRepository)
+    {
+        _stakeholderRepository = stakeholderRepository;
+    }
+
+    public async Task HandleAsync(UserRoleGranted @event)
+    {
+        var stakeholder = await _stakeholderRepository.GetAsync(@event.Id);
+        if (stakeholder is null)
+        {
+            return;
+        }
+
+        stakeholder.SetRole(@event.Role);
+        await _stakeholderRepository.UpdateAsync(stakeholder);
+    }
+}
