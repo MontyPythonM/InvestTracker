a57f57d0c4390fbd1006756ed6017e58c24a24de
Mateusz Szewczyk <mat.szewczyk96@gmail.com>
2023-10-08 14:26:56 +0200

    Add CollaborationCancelled event handler in InvestmentStrategy module. Implement collaboration repository

---
+++ src/Modules/InvestmentStrategies/src/InvestTracker.InvestmentStrategies.Application/Collaborations/Events/External/Handlers/CollaborationCancelledHandler.cs
@@ -0,0 +1,25 @@
+﻿using InvestTracker.InvestmentStrategies.Domain.Collaborations.Repositories;
+using InvestTracker.Shared.Abstractions.IntegrationEvents;
+
+namespace InvestTracker.InvestmentStrategies.Application.Collaborations.Events.External.Handlers;
+
+internal sealed class CollaborationCancelledHandler : IEventHandler<CollaborationCancelled>
+{
+    private readonly ICollaborationRepository _collaborationRepository;
+
+    public CollaborationCancelledHandler(ICollaborationRepository collaborationRepository)
+    {
+        _collaborationRepository = collaborationRepository;
+    }
+
+    public async Task HandleAsync(CollaborationCancelled @event)
+    {
+        var collaboration = await _collaborationRepository.GetAsync(@event.AdvisorId, @event.InvestorId);
+        if (collaboration is null)
+        {
+            return;
+        }
+
+        await _collaborationRepository.DeleteAsync(collaboration);
+    }
+}
--- src/Modules/InvestmentStrategies/src/InvestTracker.InvestmentStrategies.Infrastructure/Persistence/Repositories/CollaborationRepository.cs
+++ src/Modules/InvestmentStrategies/src/InvestTracker.InvestmentStrategies.Infrastructure/Persistence/Repositories/CollaborationRepository.cs
@@ -1,23 +1,42 @@
 ﻿using InvestTracker.InvestmentStrategies.Domain.Collaborations.Entities;
 using InvestTracker.InvestmentStrategies.Domain.Collaborations.Repositories;
 using InvestTracker.InvestmentStrategies.Domain.Stakeholders.ValueObjects.Types;
+using Microsoft.EntityFrameworkCore;
 
 namespace InvestTracker.InvestmentStrategies.Infrastructure.Persistence.Repositories;
 
 internal sealed class CollaborationRepository : ICollaborationRepository
 {
-    public Task<Collaboration> GetAsync(StakeholderId advisorId, StakeholderId principalId, CancellationToken token = default)
+    private readonly InvestmentStrategiesDbContext _context;
+
+    public CollaborationRepository(InvestmentStrategiesDbContext context)
     {
-        throw new NotImplementedException();
+        _context = context;
     }
 
-    public Task<bool> ExistsAsync(StakeholderId advisorId, StakeholderId principalId, CancellationToken token = default)
+    public async Task<Collaboration?> GetAsync(StakeholderId advisorId, StakeholderId principalId, CancellationToken token = default)
     {
-        throw new NotImplementedException();
+        return await _context.Collaborations
+            .SingleOrDefaultAsync(collaboration => collaboration.AdvisorId == advisorId && 
+                                                   collaboration.PrincipalId == principalId, token);
     }
 
-    public Task AddAsync(Collaboration collaboration, CancellationToken token = default)
+    public async Task<bool> ExistsAsync(StakeholderId advisorId, StakeholderId principalId, CancellationToken token = default)
     {
-        throw new NotImplementedException();
+        return await _context.Collaborations
+            .AnyAsync(collaboration => collaboration.AdvisorId == advisorId && 
+                                       collaboration.PrincipalId == principalId, token);
+    }
+
+    public async Task AddAsync(Collaboration collaboration, CancellationToken token = default)
+    {
+        await _context.Collaborations.AddAsync(collaboration, token);
+        await _context.SaveChangesAsync(token);
+    }
+
+    public async Task DeleteAsync(Collaboration collaboration, CancellationToken token = default)
+    {
+        _context.Collaborations.Update(collaboration);
+        await _context.SaveChangesAsync(token);    
     }
 }
