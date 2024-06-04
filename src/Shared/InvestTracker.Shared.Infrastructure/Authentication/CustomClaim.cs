44f274944f480e0e5b44c79499a1917c0bc07f8a
Mateusz Szewczyk <mat.szewczyk96@gmail.com>
2024-06-04 22:52:14 +0200

    Change role claim to custom

--- src/Shared/InvestTracker.Shared.Infrastructure/Authentication/CustomClaim.cs
+++ src/Shared/InvestTracker.Shared.Infrastructure/Authentication/CustomClaim.cs
@@ -2,6 +2,7 @@
 
 internal static class CustomClaim
 {
-    public const string Subscription = "subscription";
+    public const string Subscription = "system_subscription";
+    public const string Role = "system_role";
     public const string Permissions = "permissions";
 }
